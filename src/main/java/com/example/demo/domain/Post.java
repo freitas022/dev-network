6dc8523e2d3fc34f0af856cb0ae7c621187a0d1f
Matheus Freitas <mf022rj@gmail.com>
2023-01-20 19:33:22 -0300

    Criando post com User aninhado

---
+++ src/main/java/com/example/demo/domain/Post.java
@@ -0,0 +1,48 @@
+package com.example.demo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class Post implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String id;
+	private Date date;
+	private String title;
+	private String body;
+	private User author;
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
