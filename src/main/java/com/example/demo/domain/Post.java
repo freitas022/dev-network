dbadf37abdbda444b4bb6a24b6d58a7b2e4c4c6b
Matheus Freitas <mf022rj@gmail.com>
2023-01-21 10:08:38 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/example/demo/domain/Post.java
+++ src/main/java/com/example/demo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.example.demo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.example.demo.dto.AuthorDTO;
+import com.example.demo.dto.CommentDTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,23 +20,35 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Document
 public class Post implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
-	
+
+	@Getter
+	private List<CommentDTO> comments = new ArrayList<>();
+
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.title = title;
+		this.body = body;
+		this.author = author;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -45,6 +60,5 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
---
+++ src/main/java/com/example/demo/dto/CommentDTO.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CommentDTO implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String text;
+	private Date date;
+	private AuthorDTO auhor;
+	
+}
