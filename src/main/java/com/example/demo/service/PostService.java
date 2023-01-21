c6907b5132eadb070528dd1ff0101997db1d90f9
Matheus Freitas <mf022rj@gmail.com>
2023-01-21 09:56:24 -0300

    Buscando post por id

---
+++ src/main/java/com/example/demo/service/PostService.java
@@ -0,0 +1,23 @@
+package com.example.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Post;
+import com.example.demo.repository.PostRepository;
+import com.example.demo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+
+}
